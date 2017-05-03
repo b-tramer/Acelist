40571cb5169b9d4f36d3d06ce9a56e40e748d5af
b-tramer <tramerbrad@gmail.com>
2017-05-03 17:00:55 -0400

    Getting ALL facebook info

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ActiveRecord::Base
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
+      user.image = auth.info.image
+      user.email = auth.info.email
+      user.city = auth.info.city
+      user.state = auth.info.state
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
--- react/src/components/UserInfo.js
+++ react/src/components/UserInfo.js
@@ -4,18 +4,18 @@ import ReactDOM from 'react-dom';
 class UserInfo extends Component {
 
   render() {
-    // <h3> {this.props.user.name} </h3>
-
+    let profilePicture = this.props.user.image + "/picture?type=large"
     return(
       <div className="profile-container">
 
         <div className="row">
           <div className="large-1 columns" id="profile-picture">
-          <img src="  https://avatars0.githubusercontent.com/u/23392808?v=3&u=95f0fe11c5749aaa695ba9a757d48beaaca6ce2a&s=400"/>
-
+          <img src={profilePicture}/>
           </div>
 
           <div className="large-8 large-offset-3 columns" id="user-name">
+          <h3> {this.props.user.name} </h3>
+          <p> {this.props.user.city} {this.props.user.state} </p>
           </div>
         </div>
 
