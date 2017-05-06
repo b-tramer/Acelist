63fbde14421fdf48f6623741a42c4077a87bb413
b-tramer <tramerbrad@gmail.com>
2017-05-05 21:22:21 -0400

    Media queries, profile picture signed in

--- react/src/components/InfoArea.js
+++ react/src/components/InfoArea.js
@@ -25,26 +25,26 @@ import ReactDOM from 'react-dom';
       let userId = this.state.user
       return(
         <div>
-        <div className="row" id="info-area">
-        <div className="small-12 large-4 columns">
-        <img src="./search.svg" height="90" width="90"/>
-        <h5> Search Movies & Shows </h5>
-        </div>
+          <div className="row" id="info-area">
+            <div className="small-12 large-4 columns">
+              <img src="./search.svg" height="90" width="90"/>
+            <h5> Search Movies & Shows </h5>
+          </div>
 
-        <div className="small-12 large-4 columns">
-        <img src="./list.svg" height="90" width="90"/>
-        <h5> Create Custom Lists </h5>
-        </div>
+          <div className="small-12 large-4 columns">
+            <img src="./list.svg" height="90" width="90"/>
+            <h5> Create Custom Lists </h5>
+          </div>
 
-        <div className="small-12 large-4 columns">
-        <img src="./share.svg" height="90" width="90"/>
-        <h5> Save & Share With Friends </h5>
-        </div>
-        </div>
+          <div className="small-12 large-4 columns">
+            <img src="./share.svg" height="90" width="90"/>
+            <h5> Save & Share With Friends </h5>
+          </div>
+          </div>
 
-        <div id="started-area">
-        <Link to={`/users/${userId}`}> <h6> <button type="button" className="button" id="get-started-button-info">GET STARTED</button> </h6> </Link>
-        </div>
+          <div id="started-area">
+            <Link to={`/users/${userId}`}> <h6> <button type="button" className="button" id="get-started-button-info">GET STARTED</button> </h6> </Link>
+          </div>
         </div>
       )
     }
--- react/src/components/SearchBox.js
+++ react/src/components/SearchBox.js
@@ -4,21 +4,24 @@ import ReactDOM from 'react-dom';
 class SearchBox extends Component {
   render() {
     return (
-      <div className="search">
+      <div>
 
-        <form className="searchbox">
 
-          <label id="search-label" className={this.props.showCreateClass}>
-            <input onChange={this.props.handleListNameChange} type="text" placeholder="Name Your List..." id="media-input-box" value={this.props.listNameValue} />
-            <input type="submit" value="Create" id="media-search-button" onClick={this.props.listNameSubmit}/>
-          </label>
+        <div >
+          <div className="search">
+            <form className="searchbox">
+              <label id="search-label" className={this.props.showCreateClass}>
+                <input onChange={this.props.handleListNameChange} type="text" placeholder="Name Your List..." id="media-input-box" value={this.props.listNameValue} />
+                <input type="submit" value="Create" id="media-search-button" onClick={this.props.listNameSubmit}/>
+              </label>
 
-          <label id="search-label">
-            <input onChange={this.props.handleTitleChange} type="text" placeholder="Search Movies & Shows..." id="media-input-box" value={this.props.mediaValue} />
-            <input onClick={this.props.handleTitleSubmit} type="submit" value="Add" id="media-search-button"/>
-          </label>
-
-        </form>
+              <label id="search-label">
+                <input onChange={this.props.handleTitleChange} type="text" placeholder="Search Movies & Shows..." id="media-input-box" value={this.props.mediaValue} />
+                <input onClick={this.props.handleTitleSubmit} type="submit" value="Add" id="media-search-button"/>
+              </label>
+            </form>
+          </div>
+        </div>
 
       </div>
     )
