0b8668f21c805b4ec7cf56c1dd17f43308c3924b
b-tramer <tramerbrad@gmail.com>
2017-05-07 10:04:09 -0400

    Top bar sticky, fixed z index hero

--- react/src/containers/HeroContainer.js
+++ react/src/containers/HeroContainer.js
@@ -7,7 +7,22 @@ class HeroContainer extends Component{
     super(props);
     this.state = {}
   }
+
+  componentDidMount() {
+    this.getUserData()
+  }
+
+  // get current users id so that when 'get started' button is clicked, it either directs to their profile or, if they aren't signed in, the ability to sign in or sign up
+  getUserData() {
+    fetch(`/api/v1/users`, { credentials: 'same-origin' })
+      .then(response => response.json())
+      .then(responseData => {
+        this.setState({ user: responseData.current_user.id })
+    });
+  }
+
   render() {
+    let userId = this.state.user
     return(
       <div>
         <div className='information'>
@@ -15,7 +30,9 @@ class HeroContainer extends Component{
               <div className='hero-text'>
                 <h1> Life’s Better Listed </h1>
                 <h4> Rank & share your favorite movies & shows </h4>
+                <Link to={`/users/${userId}`}>
                <button type="button" className="button" id="get-started-button">GET STARTED</button>
+               </Link>
               </div>
           </div>
         </div>
