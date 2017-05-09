7db761b36e1db73e0adf504f982935951ab9df78
b-tramer <tramerbrad@gmail.com>
2017-05-08 21:55:31 -0400

    Heroku screencast

--- react/src/components/InfoArea.js
+++ react/src/components/InfoArea.js
@@ -23,27 +23,30 @@ import ReactDOM from 'react-dom';
 
     render() {
       let userId = this.state.user
+      // <div id="started-area">
+      //   <Link to={`/users/${userId}`}> <h6> <button type="button" className="button" id="get-started-button-info">GET STARTED</button> </h6> </Link>
+      // </div>
       return(
         <div>
           <div className="row" id="info-area">
-            <div className="small-12 large-4 columns">
-              <img src="./search.svg" height="90" width="90"/>
-            <h5> Search For Media </h5>
+
+          <div className="small-12 large-6 columns">
+            <img src={assetHelper["acelist-gif2.gif"]} height="600" width="600" id='home-gif'/>
           </div>
 
-          <div className="small-12 large-4 columns">
+          <div className="small-12 large-6 columns">
+            <img src="./search.svg" height="90" width="90"/>
+            <h5> Search For Media </h5>
             <img src="./list.svg" height="90" width="90"/>
             <h5> Create Custom Lists </h5>
-          </div>
-
-          <div className="small-12 large-4 columns">
             <img src="./share.svg" height="90" width="90"/>
             <h5> Share With Friends </h5>
           </div>
-          </div>
 
+
+        </div>
           <div id="started-area">
-            <Link to={`/users/${userId}`}> <h6> <button type="button" className="button" id="get-started-button-info">GET STARTED</button> </h6> </Link>
+          <Link to={`/users/${userId}`}> <h6> <button type="button" className="button" id="get-started-button-info">GET STARTED</button> </h6> </Link>
           </div>
         </div>
       )
