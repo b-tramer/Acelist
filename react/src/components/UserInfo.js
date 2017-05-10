d4e1254fae5f49180986256e79c5afbc8d0e61cc
b-tramer <tramerbrad@gmail.com>
2017-05-09 22:22:36 -0400

    heroku

--- react/src/components/UserInfo.js
+++ react/src/components/UserInfo.js
@@ -95,7 +95,7 @@ class UserInfo extends Component {
     let userSignedIn;
     let profilePicture;
     let nameDisplay;
-    if (this.props.user.id === this.props.current_user.id) {
+    if (this.props.user.image) {
       userNotSignedIn = 'hidden'
       userSignedIn = 'show'
       profilePicture = this.props.user.image + "/picture?type=large"
